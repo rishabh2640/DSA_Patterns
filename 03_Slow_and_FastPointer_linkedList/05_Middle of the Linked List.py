d3a4f6e37d0d080b5c405c81b4fe26c0a23bce44
Rishabh <rishabh26402@gmail.com>
2026-01-06 12:22:39 +0530

    finding middle link list

---
+++ 03_Slow_and_FastPointer_linkedList/05_Middle of the Linked List.py
@@ -0,0 +1,52 @@
+# 876. Middle of the Linked List
+
+# Given the head of a singly linked list, return the middle node of the linked list.
+# If there are two middle nodes, return the second middle node.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+# Example 2:
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+# Definition for singly-linked list.
+
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            print(slow.val)
+            fast = fast.next.next
+            print(fast.val)
+        
+        return slow.val
+    
+solver = Solution()
+
+head = [1,2,3,4,5]
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+node5 = ListNode(5)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
+
+print(solver.middleNode(node1))
