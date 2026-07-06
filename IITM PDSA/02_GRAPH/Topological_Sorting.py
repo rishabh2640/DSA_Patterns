d5857ebbf5f44648ff97f9ea1fd339920317b9a7
Rishabh <rishabh26402@gmail.com>
2026-07-06 09:49:15 +0530

    Refactor Topological Sorting implementation and add adjacency list version

--- IITM PDSA/02_GRAPH/Topological_Sorting.py
+++ IITM PDSA/02_GRAPH/Topological_Sorting.py
@@ -1,5 +1,7 @@
 # Directed Acyclic Graph (DAG) : Topological Sorting with Adjacency Matrix [taking Time complexity of O(n^2)]
 import numpy as np
+from myQueue import Queue
+
 Adj_Mat = np.array([
     [0, 1, 1, 0],  # Node 0 goes to 1, 2
     [0, 0, 0, 1],  # Node 1 goes to 3
@@ -10,7 +12,7 @@ Adj_Mat = np.array([
 def topoSort(AMat):
     rows, cols = AMat.shape
     indegree = {}
-    toposort = []
+    topoSortList = []
 
     for c in range(cols):
         indegree[c] = 0
@@ -20,13 +22,57 @@ def topoSort(AMat):
     
     for i in range(rows):
         j = min([k for k in range(cols) if indegree[k] == 0])
-        toposort.append(j)
+        topoSortList.append(j)
         indegree[j] = indegree[j] - 1
 
         for k in range(cols):
             if AMat[j,k] == 1:
                 indegree[k] = indegree[k] - 1
     
-    return toposort
+    return topoSortList
 
 print(topoSort(Adj_Mat))
+
+
+# Directed Acyclic Graph (DAG) : Topological Sorting with Adjacency List [ Time complexity of O( m+n )]
+
+adj_list = {
+    0: [2, 3, 4],
+    1: [3, 5],
+    2: [4, 7],
+    3: [5,],
+    4: [6,],
+    5: [6, 7],
+    6: [],
+    7: []
+}
+def topoSortAdjList(AList):
+    indegree2, topoSortList2 = {}, []
+    zeroDegreeQ = Queue()
+
+    for i in AList.keys():
+        indegree2[i] = 0
+
+    for u in AList.keys():
+        for v in AList[u]:
+            indegree2[v] = indegree2[v] + 1
+    
+    for u in AList.keys():
+        if indegree2[u] == 0:
+            zeroDegreeQ.addq(u)
+    
+    while not zeroDegreeQ.isempty():
+        j = zeroDegreeQ.delq()
+        topoSortList2.append(j)
+        indegree2[j] = indegree2[j] - 1
+
+        for k in AList[j]:
+            indegree2[k] = indegree2[k] - 1
+            if indegree2[k] == 0:
+                zeroDegreeQ.addq(k)
+    return topoSortList2
+
+
+
+print(topoSortAdjList(adj_list))
+
