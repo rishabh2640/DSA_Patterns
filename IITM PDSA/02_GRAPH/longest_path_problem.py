4510db0d96be8b4fdb0952f6de718ab83b32402b
Rishabh <rishabh26402@gmail.com>
2026-07-07 15:20:01 +0530

    Implement recursive approach to find the longest path in a directed acyclic graph

--- IITM PDSA/02_GRAPH/longest_path_problem.py
+++ IITM PDSA/02_GRAPH/longest_path_problem.py
@@ -83,3 +83,62 @@ def longJourney(AList):
     return route
 
 print( longJourney(AdjList))
+
+
+# another way using recursion
+
+def longJourney(AList):
+    memo = {}  # city -> best route (list of cities) starting from that city
+
+    def longest_from(city):
+        if city in memo:            # already solved this city before? reuse it
+            return memo[city]
+
+        best_route = [city]         # worst case: dead end, route is just this city
+        for neighbour in AList.get(city, []):
+            route = [city] + longest_from(neighbour)   # try going to this neighbour
+            if len(route) > len(best_route):
+                best_route = route                       # keep it if it's longer
+
+        memo[city] = best_route
+        return best_route
+
+    # Try every city as a starting point, return whichever route is longest
+    return max((longest_from(city) for city in AList), key=len)
+
+# another way using recursion but little expended version
+
+def longJourney(AList):
+    """
+    DAG (Directed Acyclic Graph) mein longest path find karne ka function.
+    """
+    # Memoization dictionary jo har city ka longest possible path store karegi
+    memo = {}
+    
+    def dfs(city):
+        # Agar is city ka longest path pehle se pata hai, toh wahi return kardo
+        if city in memo:
+            return memo[city]
+            
+        max_path = []
+        # Padosi cities (neighbors) mein traverse karo
+        for padosi in AList.get(city, []):
+            path = dfs(padosi)
+            # Agar naya path purane max_path se lamba hai, toh isko update karo
+            if len(path) > len(max_path):
+                max_path = path
+                
+        # Current city ko longest sub-path ke start mein jod do
+        memo[city] = [city] + max_path
+        return memo[city]
+        
+    sabse_lamba_raasta = []
+    
+    # Graph ki har ek city ko starting point maan kar check karo
+    for city in AList:
+        current_path = dfs(city)
+        # Jiska route sabse lamba ho usko save kar lo
+        if len(current_path) > len(sabse_lamba_raasta):
+            sabse_lamba_raasta = current_path
+            
+    return sabse_lamba_raasta
