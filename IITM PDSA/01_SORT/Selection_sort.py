6872e75378c2d4b07086470f529b306afdb4096f
Rishabh <rishabh26402@gmail.com>
2026-07-13 11:39:33 +0530

    Fix comments in selection_sort.py for clarity and correctness

--- IITM PDSA/01_SORT/Selection_sort.py
+++ IITM PDSA/01_SORT/Selection_sort.py
@@ -1,8 +1,8 @@
-# How it works: It divides the list into a "sorted" section and an "unsorted" section. It scans the entire unsorted section to find the absolute minimum value, and swaps it with the first unsorted element.Best 
+# How it works: It divides the list into a "sorted" section and an "unsorted" section. It scans the entire unsorted section to find the absolute minimum value, and swaps it with the first unsorted element. 
 
-# for: Situations where writing to memory is very expensive, because it performs the minimum possible number of swaps (at most $O(n)$ swaps).
+# Best for: Situations where writing to memory is very expensive, because it performs the minimum possible number of swaps (at most $O(n)$ swaps).
 
-# Key Trait: It scans the whole remaining list before making a single swap.
+# Key Trait: It scans the whole remaining list before making a single swap. It does not do stable sorting and in place.
 
 def selection_sort(arr):
     # store the length of the array
