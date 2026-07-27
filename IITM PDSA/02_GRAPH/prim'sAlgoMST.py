f37f3c4924b6fa90e452187bf85b75d8e29d5ae4
Rishabh <rishabh26402@gmail.com>
2026-07-27 12:34:34 +0530

    variable renaming

--- IITM PDSA/02_GRAPH/prim'sAlgoMST.py
+++ IITM PDSA/02_GRAPH/prim'sAlgoMST.py
@@ -33,8 +33,8 @@ def primList(WList):
 
     return TE
 
-dedges = [(0,1,10),(0,3,18),(1,2,20),(1,3,6),(2,4,8),(3,4,70)]
-edges = dedges + [(j,i,w) for (i,j,w) in dedges]
+dEdges = [(0,1,10),(0,3,18),(1,2,20),(1,3,6),(2,4,8),(3,4,70)]
+edges = dEdges + [(j,i,w) for (i,j,w) in dEdges]
 size = 5
 WL = {}
 for i in range(size):
